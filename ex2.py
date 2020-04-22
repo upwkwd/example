0b6846f1abba2e65a2a952634e3f12ba33e7e1cb
shubham sinha <63578381+upwkwd@users.noreply.github.com>
2020-04-22 22:17:10 +0530

    resolved a problem

--- ex2.py
+++ ex2.py
@@ -1,18 +1,19 @@
 def skip_elements(elements):
 	# Initialize variables
 	new_list = []
-	i = 0
+	a = len(elements)+1
+	if a ==1:
+		return new_list
+	else:
+		for i in range(0, a):
 
-	# Iterate through the list
-	for element in skip_elements():
-		# Does this element belong in the resulting list?
-		if skip_elements[i] in skip_elements == True:
-			# Add this element to the resulting list
-			new_list.append(skip_elements[i])
-		# Increment i
-		i += 2
+            if i%2==0:
 
-	return new_list
+                new_list.append(elements[i])
+
+        return new_list
+
+	
 
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
 print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
