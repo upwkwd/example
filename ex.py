5d035cfab9189abf7c057a972e68b6575a83a121
upwkwd <63578381+upwkwd@users.noreply.github.com>
2020-04-22 12:05:23 +0530

    create a ex.py

---
+++ ex.py
@@ -0,0 +1,11 @@
+def initials(phrase):
+    words = phrase.___
+    result = ""
+    for word in words:
+        result += ___
+    return ___
+
+    
+print(initials("Universal Serial Bus")) # Should be: USB
+print(initials("local area network")) # Should be: LAN
+print(initials("Operating system")) # Should be: OS
